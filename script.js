44c364838a44a0a80efddcaf39f70c3e42d45a91
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-10 02:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	for(let i=0;i<arr.length;i++){
+        sm+=arr[i];
+        if(sm>n){
+            sm=arr[i];
+            ans.push(tmp);
+            tmp=[];
+        }
+        tmp.push(arr[i]);
+    }
+    ans.push(tmp);
+    return ans;
+	
 };
 
 const n = prompt("Enter n: ");
