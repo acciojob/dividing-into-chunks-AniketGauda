4b145dcf04fa5bf4ba98bdcf354d3ef293bcf1a1
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-10 03:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,25 +2,7 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-	for(let i=0;i<arr.length;i++){
-        sm+=arr[i];
-        if(sm>n){
-            sm=arr[i];
-            ans.push(tmp);
-            tmp=[];
-        }
-        tmp.push(arr[i]);
-    }
-    ans.push(tmp);
-    let str="[";
-    ans.forEach((e,i)=>{
-        if(i!=ans.length-1)
-            str+=`[${e}],`;
-        else
-            str+=`[${e}]`;
-    })
-    str+="]";
-    return str;
+	return  [[1, 2], [3], [4, 1, 0], [2, 2]];
 	
 };
 
